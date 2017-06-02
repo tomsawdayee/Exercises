4c8cff89dabbd7a0d43d4c5479030a0832fac8ca
Tom <firez03@gmail.com>
2017-06-02 03:50:33 +0300

    Added AllUniqueChars

---
+++ Exercises/AllUniqueCharacters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllUniqueCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var allUniqueChars = new StringAllUniqueChars();
+
+            var str = "faxfgnac";
+            Console.WriteLine(allUniqueChars.WithAdditionalMemory(str));
+            Console.WriteLine(allUniqueChars.WithoutAdditionalMemory(str));
+        }
+    }
+}
---
+++ Exercises/AllUniqueCharacters/StringAllUniqueChars.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllUniqueCharacters
+{
+    public class StringAllUniqueChars
+    {
+        public bool WithAdditionalMemory(string str) {
+
+            var dict = new Dictionary<char, bool>();
+
+            foreach (var chr in str){
+                if (dict.ContainsKey(chr))
+                    return false;
+                dict.Add(chr, true);
+            }
+
+            return true;
+        }
+
+        public bool WithoutAdditionalMemory(string str)
+        {
+            str = string.Concat(str.OrderBy(c => c));
+
+            for (var i = 0; i < str.Length - 1; i++)
+            {
+                if (str[i] == str[i + 1])
+                    return false;
+            }
+            
+            return true;
+        }
+    }
+}
